b72ff0956e027e0af09afc736b35d8c94bdc9e8a
unknown <virocha.tds2.senai@gmail.com>
2024-03-12 14:55:35 -0300

    Nenhuma mudança feita

---
+++ AulaFor.js
@@ -0,0 +1,10 @@
+//Não tenho a instancia da variavel fora da sintaxe
+
+for (let i = 1; i <= 10; i++) { 
+    //aqui dentro vai a minha lógica
+    //nao tenho mais o i++ ou i-- aqui dentro, fica na sintaxe
+    console.log(i);
+}
+
+//mostro que terminou
+console.log("Terminou o For!!!");
