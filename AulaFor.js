858147d6c1a00b618c3325206e7cf6f63288fa2e
Vinícius Rocha <158187426+Vinirocha388@users.noreply.github.com>
2024-03-12 16:55:05 -0300

    Update AulaFor.js

--- AulaFor.js
+++ AulaFor.js
@@ -1,7 +1,9 @@
 for (let i = 1; i <= 10; i++) { 
    "Minha lógica aqui" 
-    //Não tenho mais o i++ ou i-- aqui dentro, fica na sintaxe
+   
+   //Não tenho mais o i++ ou i-- aqui dentro, fica na sintaxe
     console.log(i);
 }
+
 //Exibo que terminou 
 console.log("Fim do loop FOR");
