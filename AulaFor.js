7eac1e203a9989b16e8ff3171ed61228f53b44dc
unknown <virocha.tds2.senai@gmail.com>
2024-03-12 15:26:17 -0300

    Linha 4 na forma original

--- AulaFor.js
+++ AulaFor.js
@@ -1,7 +1,7 @@
 //Não tenho a instancia da variavel fora da sintaxe
 
 for (let i = 1; i <= 10; i++) { 
-    //aqui dentro vai a minha lógica
+  //Minha lógica vai aquiii
     //nao tenho mais o i++ ou i-- aqui dentro, fica na sintaxe
     console.log(i);
 }
