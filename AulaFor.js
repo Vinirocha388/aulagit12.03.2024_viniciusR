367be9ba6dbe3248cfb09887965546e63885b561
Vinícius Rocha <158187426+Vinirocha388@users.noreply.github.com>
2024-03-12 15:17:46 -0300

    Update AulaFor.js
    Volta da linha 4

--- AulaFor.js
+++ AulaFor.js
@@ -1,7 +1,7 @@
 //Não tenho a instancia da variavel fora da sintaxe
 
 for (let i = 1; i <= 10; i++) { 
-    
+   "Minha lógica aqui" 
     //nao tenho mais o i++ ou i-- aqui dentro, fica na sintaxe
     console.log(i);
 }
